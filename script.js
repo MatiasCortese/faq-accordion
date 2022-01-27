d3b40f8a603fa2434bab9e4916659e64fffc305e
Matias Cortese <matiasemanuelcortese@gmail.com>
2022-01-27 13:01:53 -0300

    v0.1 - Pending arrow and answer animation & responsive stuff

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Save questions
+const questions = document.getElementsByClassName("question")
+
+// We loop through the array of questions so we can add an event listener on each question and set a function as response to the event
+Array.from(questions).forEach(question => {
+    question.addEventListener('click', open)
+})
+
+// This function manages the answer and the arrow
+function open(event) {
+    Array.from(questions).forEach(question => {
+        // If the question clicked is the one we are looping through
+        if (event.target.innerHTML == question.innerHTML) {
+            // If the question clicked doesn't have active class
+            if (!question.classList.contains('active'))
+            {
+                // Add active state to the question
+                question.classList.add("active")
+                // Add shown state to the answer
+                question.nextElementSibling.classList.add('shown')
+                // Turn arrow looking up
+            } 
+            // If the question clicked already has active class
+            else {
+                // Remove question active state
+                question.classList.remove('active')
+                // Remove answer shown state
+                question.nextElementSibling.classList.remove('shown') 
+                // Turn arrow looking down again
+            }
+        }
+    })
+}
+
